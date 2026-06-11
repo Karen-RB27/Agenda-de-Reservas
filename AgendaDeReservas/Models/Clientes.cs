20ada9e34e1d8b295a7a850429a32200ea9452f3
Karen <rochabrigagaokaren@gmail.com>
2026-06-11 10:01:23 -0300

    clientes

---
+++ AgendaDeReservas/Models/Clientes.cs
@@ -0,0 +1,12 @@
+namespace AgendaDeReservas.Models
+{
+    public class Clientes
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        
+    }
+}
