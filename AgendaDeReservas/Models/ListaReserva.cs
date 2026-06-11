98aeac0fc4f11b0f50c5ff7b99819b19bebf3af9
Karen <rochabrigagaokaren@gmail.com>
2026-06-11 11:34:03 -0300

    Lista Reserva

---
+++ AgendaDeReservas/Models/ListaReserva.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+namespace AgendaDeReservas.Models
+{
+    public static class ListaReserva
+    {
+        private static List<Reserva> reservas = new List<Reserva>
+        {
+            new Reserva
+            {
+                Id = 1,
+                NomeCliente = "Karen Rocha",
+                Telefone = "991782199",
+                DataReserva = DateTime.Parse("2026-10-27"),
+                TipoEvento = "Aniversário",
+                Observacao = "50 pessoas",
+                Horario = "12:00"
+            },
+
+            new Reserva
+            {
+                Id = 2,
+                NomeCliente = "Kauany Brigagão",
+                Telefone = "993056052",
+                DataReserva = DateTime.Parse("2026-10-28"),
+                TipoEvento = "Aniversário",
+                Observacao = "100 pessoas",
+                Horario = "20:00"
+            }
+        };
+
+        public static List<Reserva> ObterTodas()
+        {
+            return reservas;
+        }
+    }
+}
---
+++ AgendaDeReservas/Models/Reserva.cs
@@ -0,0 +1,15 @@
+namespace AgendaDeReservas.Models
+
+{
+    public class Reserva
+    {
+        public int Id { get; set; }
+        public string NomeCliente { get; set; }
+        public int Telefone { get; set; }
+        public DateTime DataReserva { get; set; }
+        public string TipoDeEvento { get; set; }
+        public string Observacao { get; set; }
+        public string Horario { get; set; }
+        
+    }
+}
