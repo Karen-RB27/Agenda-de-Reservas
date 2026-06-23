e622853341195e896e50d0bec5346adaee215235
Karen <rochabrigagaokaren@gmail.com>
2026-06-23 12:05:13 -0300

    BD

---
+++ AgendaDeReservas/DATA/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using AgendaDeReservas.Models;
+
+namespace AgendaDeReservas.DATA
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Reserva> Reservas { get; set; }
+    }
+}
