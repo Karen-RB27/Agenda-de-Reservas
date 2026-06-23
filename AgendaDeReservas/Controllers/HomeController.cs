3b755479ccc078627560f16dc47cdc088519f60b
Karen <rochabrigagaokaren@gmail.com>
2026-06-23 09:36:02 -0300

    contato

--- AgendaDeReservas/Controllers/HomeController.cs
+++ AgendaDeReservas/Controllers/HomeController.cs
@@ -21,4 +21,14 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    public IActionResult Sobre()
+    {
+        return View();
+    }
+
+    public IActionResult Contato()
+    {
+        return View();
+    }
 }
--- AgendaDeReservas/obj/Debug/net10.0/AgendaDeReservas.AssemblyInfo.cs
+++ AgendaDeReservas/obj/Debug/net10.0/AgendaDeReservas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgendaDeReservas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0275f7f8b0f0d8d0e0d889e275f6b2ad2f67b3f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27755e40f58f7fbbb4393cf465728e6b05c5892d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgendaDeReservas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgendaDeReservas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
